c75ef118c5544d710183f2e1f6afa88c480c44e7
Kurt-Urban <Kurt-Urban@users.noreply.github.com>
2022-01-17 19:42:38 +0000

    deploy: 1418bd1bb64b2ab3789929ad2c70703b63f7da87

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? "/Kurt-Urban.github.io/" : "",
-};
-
-module.exports = nextConfig;
