31d48ff075c5786652dc4ee45cae4655f5006e71
Kurt-Urban <kurturban00@gmail.com>
2022-01-20 17:38:35 -0500

    test: changing tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  mode: "jit",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
     extend: {
       colors: {
